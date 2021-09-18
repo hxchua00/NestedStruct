b2fed2f827d0e8f0514ff19340c77798b1cf75f0
hxchua00 <hxchua00@gmail.com>
2021-09-18 21:10:02 +0800

    Add project files.

---
+++ NestedStruct/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedStruct
+{    
+    struct Company
+    {
+         public struct Employee
+        {
+            public Employee(string empName, int empID)
+            {
+                EmpName = empName;
+                EmpID = empID;
+            }
+            string EmpName { get; set; }
+            int EmpID { get; set; }
+            public void printAll()
+            {
+                Console.WriteLine("Employee Name: " + EmpName);
+                Console.WriteLine("Employee ID: " + EmpID);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company c = new Company();
+            Console.ReadLine();
+        }
+    }
+}
